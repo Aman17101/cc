378049e39cb29ca4aa457e70793b32e5d44a2dfe
Aman17101 <asingh36705@gmail.com>
2025-05-15 16:16:55 +0530

    import/export

--- importableExecutble/cmd/main.go
+++ importableExecutble/cmd/main.go
@@ -2,17 +2,24 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/Aman17101/cc/importableExecutble/calculator"
+	"github.com/Aman17101/cc/importableExecutble/store"
 )
 
 func main() {
-//	fmt.Println("sum =", calculator.Add(987872, 90909))
+	//	fmt.Println("sum =", calculator.Add(987872, 90909))
 	fmt.Println("sub =", calculator.Sub(987872, 90909))
 	fmt.Println("mul =", calculator.Mul(987872, 90909))
 	fmt.Println("div =", calculator.Div(987872, 90909))
 
-	usr:=calculator.User{}
-	usr.Name="iuidai"
-	fmt.Println("++++++",usr.Name)
+	usr := calculator.User{}
+	usr.Name = "iuidai"
+	fmt.Println("++++++", usr.Name)
+
+//import using interface
+	//var dboprs store.DBOperations
+	dboprs :=store.Store{}
+	dboprs.SaveRecord("just print this record ....")
 
 }
